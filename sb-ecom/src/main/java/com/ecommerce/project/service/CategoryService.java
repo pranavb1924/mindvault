33176a774ce150a03034a01a93ce36dc7263fd9a
Pranav B <bhoopal.p@northeastern.edu>
2025-06-14 15:47:09 -0400

    Delete sb-ecom directory
    Accidentally added folder

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.project.service;
-import com.ecommerce.project.model.Category;
-import java.util.*;
-
-public interface CategoryService {
-    List<Category> getAllCategories();
-    void createCategory(Category category);
-}
--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.ecommerce.project.service;
-
-public class CategoryServiceImpl {
-}
