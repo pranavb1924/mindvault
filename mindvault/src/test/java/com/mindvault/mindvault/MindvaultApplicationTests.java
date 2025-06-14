ee3b30009c80778dee9fce5cfca15612175d28aa
Pranav B <bhoopal.p@northeastern.edu>
2025-06-14 15:44:31 -0400

    Skeleton Structure with user model

---
+++ mindvault/src/test/java/com/mindvault/mindvault/MindvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindvault.mindvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindvaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
