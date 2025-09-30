1e228e4acecc09a576a5790dba2f54c0c42cd022
Pranav B <bhoopal.p@northeastern.edu>
2025-09-30 11:47:11 -0400

    Starting from scratch

--- backend/src/main/java/com/mindvault/mindvault/dto/UserProfile.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/mindvault/mindvault/dto/UserProfile.java
-package com.mindvault.mindvault.dto;
-
-public record UserProfile(
-        Long id,
-        String firstName,
-        String lastName,
-        String email
-) {}
